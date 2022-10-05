11eeee204a58a47cfd962d1bd430986c37ad77c5
LeoMainini <leonardo.apple.mainini@gmail.com>
2022-10-05 16:10:08 +0100

    update helper script

--- help.sh
+++ help.sh
@@ -3,12 +3,13 @@
 #                                                         :::      ::::::::    #
 #    help.sh                                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: leferrei <leferrei@student.42lisboa.c      +#+  +:+       +#+         #
+#    By: leferrei <leferrei@student.42lisboa.com>   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/10/04 17:05:19 by leferrei          #+#    #+#              #
-#    Updated: 2022/10/05 15:59:49 by leferrei         ###   ########.fr        #
+#    Created: 2022/10/05 16:01:21 by leferrei          #+#    #+#              #
+#    Updated: 2022/10/05 16:06:03 by leferrei         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-printf "This tester must be placed inside your push_swap root directory and takes no arguments.\nYou can change the test lenghts changing the LEN_n variable, its not recomended going over 50 in the 500 numbers test.\n\nTesting will include 4 sets of random numbers with lenghts of 3, 5, 100 and 500.\n\n" 
-printf "If the LOG_OUT option is ticked, once completed you can view the inputs and outputs in the test_n.log files.\n\n"
+
+printf "This tester is made to be place inside a folder placed in the root directory of push_swap or where your push_swap binary is located, but can be placed anywhere as long as the PATH variable is changed.\n\nYou can change the test lenghts changing the LEN_n variable, its not recomended going over 50 in the 500 numbers test.\n\nTesting will include 4 sets of random numbers with lenghts of 3, 5, 100 and 500.\n\n" 
+printf "If the LOG_OUT option is ticked, once completEd you can view the inputs and outputs in the test_n.log files.\n\n"
