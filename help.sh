5be1c5793335ce5f71c038c88627235fecf5e338
LeoMainini <leonardo.apple.mainini@gmail.com>
2022-10-05 16:00:04 +0100

    update helper script

--- help.sh
+++ help.sh
@@ -6,9 +6,9 @@
 #    By: leferrei <leferrei@student.42lisboa.c      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/04 17:05:19 by leferrei          #+#    #+#              #
-#    Updated: 2022/10/04 17:05:21 by leferrei         ###   ########.fr        #
+#    Updated: 2022/10/05 15:59:49 by leferrei         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-echo "This tester must be placed inside your push_swap root directory and takes no arguments.\nYou can change the test lenghts changing the LEN_n variable, its not recomended going over 50 in the 500 numbers test.\n\nTesting will include 4 sets of random numbers with lenghts of 3, 5, 100 and 500.\n"
-echo "Once completed you can view the inputs and outputs in the test_n.log files.\n"
+printf "This tester must be placed inside your push_swap root directory and takes no arguments.\nYou can change the test lenghts changing the LEN_n variable, its not recomended going over 50 in the 500 numbers test.\n\nTesting will include 4 sets of random numbers with lenghts of 3, 5, 100 and 500.\n\n" 
+printf "If the LOG_OUT option is ticked, once completed you can view the inputs and outputs in the test_n.log files.\n\n"
